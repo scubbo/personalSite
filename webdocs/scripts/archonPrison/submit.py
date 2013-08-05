490822f1305f246c54b06840e66994d2ba6b49de
Jack Jackson <scubbojj@gmail.com>
2013-08-05 12:39:20 +0100

    Close email vulnerability

--- webdocs/scripts/archonPrison/submit.py
+++ webdocs/scripts/archonPrison/submit.py
@@ -9,6 +9,8 @@ sys.stdout.write('Content-Type: text/html\r\n\r\n')
 userEmail = form.getvalue('email', 'blank')
 userName = form.getvalue('name', 'User')
 
-os.system('echo \'name was ' + userName + ' and email was ' + userEmail + '\' | mutt -s \'Archon Prison output\' scubbojj@gmail.com')
+# os.system('echo \'name was ' + userName + ' and email was ' + userEmail + '\' | mutt -s \'Archon Prison output\' scubbojj@gmail.com')
+# The above is bad and evil and wrong, and I should be very ashamed of exposing this vulnerability :)
+# I'll be replacing this with a properly-handled email call to smtplib this evening
 
-sys.stdout.write('name was ' + userName + ' and email was ' + userEmail)
+# sys.stdout.write('name was ' + userName + ' and email was ' + userEmail)
